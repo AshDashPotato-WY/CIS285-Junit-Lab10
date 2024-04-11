39736c2cd90649de17cf36c9f8b5d683b5798d59
AshDashPotato-WY <ashleyweiyu@yahoo.com>
2024-04-11 15:42:21 -0400

    add test duplicate

--- src/cis285JunitLab10/testSelectionSortTest.java
+++ src/cis285JunitLab10/testSelectionSortTest.java
@@ -69,12 +69,59 @@ public class testSelectionSortTest {
         System.out.println(Arrays.toString(outputs));
         System.out.println(Arrays.toString(Sortedarr));
         
-        assertEquals(Arrays.toString(outputs), Arrays.toString(Sortedarr));
+        assertEquals(Arrays.toString(Sortedarr), Arrays.toString(outputs));
     }
     
     public void testDuplicates(){
         
         /** Test data contains duplicates **/
+    	int[] arr1 = new int[5];
+        arr1[0] = -8;
+        arr1[1] = -9;
+        arr1[2] = -8;
+        arr1[3] = -10;
+        arr1[4] = -2;
+        
+        int[] Sortedarr1 = new int[5];
+        Sortedarr1[0] = -10;
+        Sortedarr1[1] = -9;
+        Sortedarr1[2] = -8;
+        Sortedarr1[3] = -8;
+        Sortedarr1[4] = -2;
+        
+        int[] arr2 = new int[5];
+        arr2[0] = 2;
+        arr2[1] = 6;
+        arr2[2] = 4;
+        arr2[3] = 4;
+        arr2[4] = 1;
+        
+        int[] Sortedarr2 = new int[5];
+        Sortedarr2[0] = 1;
+        Sortedarr2[1] = 2;
+        Sortedarr2[2] = 4;
+        Sortedarr2[3] = 4;
+        Sortedarr2[4] = 6;
+        
+        // test all negative numbers with duplicates
+        int[] outputs1 = new int[5];
+        outputs1 = test.basicSelectionSort(arr1);
+        
+        System.out.println(Arrays.toString(outputs1));
+        System.out.println(Arrays.toString(Sortedarr1));
+        
+        assertEquals(Arrays.toString(Sortedarr1), Arrays.toString(outputs1));
+        
+        // test all positive numbers with duplicates
+        int[] outputs2 = new int[5];
+        outputs2 = test.basicSelectionSort(arr2);
+        
+        System.out.println(Arrays.toString(outputs2));
+        System.out.println(Arrays.toString(Sortedarr2));
+        
+        assertEquals(Arrays.toString(Sortedarr2), Arrays.toString(outputs2));
+        
+        
     }
 
 
