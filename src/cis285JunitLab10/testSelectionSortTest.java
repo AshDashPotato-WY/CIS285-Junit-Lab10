2740a5c8baecd03b5bfd01784a99521bfd27f1d0
AshDashPotato-WY <ashleyweiyu@yahoo.com>
2024-04-11 14:39:08 -0400

    initial commit

---
+++ src/cis285JunitLab10/testSelectionSortTest.java
@@ -0,0 +1,14 @@
+package cis285JunitLab10;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testSelectionSortTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
